bff46a6114d05279a205c6382d67a41b9653a505
GrishUmesh <harshaukhandare+github@gmail.com>
2025-03-09 13:25:48 +0530

    Version 0.0.6

--- pushfd.bat
+++ pushfd.bat
@@ -1,5 +1,5 @@
 git add .
 git commit -m "Version 0.0.6"
-git remote add origin https://github.com/GrishmUmesh/Haunt-CMD
+git remote add origin https://github.com/GrishmUmesh/Haunt-CMD.git
 git branch -M master
 git push -u origin master
